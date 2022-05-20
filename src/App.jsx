25107941e80524ada62cf3f31635f6a6bc2910b1
TsybulskaAnna <tsybulska.an@gmail.com>
2022-05-20 13:49:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import data from './data/data.json';
 import friends from './data/friends.json';
 import transactions from './data/transactions.json';
 
-import Profile from 'Components/Profile/Profile';
-import Statistics from 'Components/Statistics/Statistics';
+import Profile from 'Components/Profile';
+import Statistics from 'Components/Statistics';
 import FriendList from 'Components/FriendList';
 import TransactionHistory from 'Components/TransactionHistory';
 
